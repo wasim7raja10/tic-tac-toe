32ba541b4527ea8ada6297d10ea1b99ce792d15e
Wasim Raja <wasim7raja10@gmail.com>
2022-11-27 00:28:53 +0530

    created player objects with factor functions

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const square = Array.from(document.querySelectorAll('.square'));
 const playerTurn = document.querySelector('.player-turn');
 const resetButton = document.querySelector('.restart');
+
+const playerFactory = (name, symbol) => {
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  return { getName, getSymbol };
+}
+
+const player1 = playerFactory('Player 1', 'X');
+const player2 = playerFactory('Player 2', 'O');
